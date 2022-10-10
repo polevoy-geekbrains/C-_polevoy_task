3ac10888c5611eef66676eebfccebbe9c35c6a17
Aleksandr Polevoy <polevoyaa@gmail.com>
2022-10-10 18:33:48 +0300

    task_06

---
+++ task_06_01/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 41: 
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/ 
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 1)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число больше нуля. {userInformation} ");
+    }
+    return result;
+}
+
+int countPositiveNumbers(int userNumber)
+{
+int count = 0;
+for (int i = 0; i < userNumber; i++)
+   {
+    Console.Write($"Введите число {i + 1}:\t");
+    int userNumber1 = Convert.ToInt32(Console.ReadLine());
+    if (userNumber1 > 0)
+            count++;
+   }
+return count;
+}
+int tempNumber = getNumberFromUser("Введите количество вводимых чисел:\t");
+int userNumber = countPositiveNumbers(tempNumber);
+Console.Write($"Положительных чисел: {userNumber}");
---
+++ task_06_02/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задача 43: 
+Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+double[,] coeff = new double[2, 2];
+double[] IntersectionPoint = new double[2];
+
+void InputValues(){
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Solution(double[,] coeff)
+{
+  IntersectionPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  IntersectionPoint[1] = IntersectionPoint[0] * coeff[0,0] + coeff[0,1];
+  return IntersectionPoint;
+}
+
+void OutputSolution(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"Прямые совпадают");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"Прямые параллельны");
+  }
+  else 
+  {
+    Solution(coeff);
+    Console.Write($"Точка пересечения прямых: ({IntersectionPoint[0]}, {IntersectionPoint[1]})");
+  }
+}
+
+InputValues();
+OutputSolution(coeff);
